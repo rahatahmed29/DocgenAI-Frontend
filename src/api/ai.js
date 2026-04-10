3e01e6051dbc6fd42408ef3f9baaad5fdb541163
Rahat Ahmed Khan <rahatofficial.29@gmail.com>
2026-04-10 20:03:42 +0600

    edited the server url

--- src/api/ai.js
+++ src/api/ai.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export default async function annotateCode(code,style="jsdoc"){
    
-        const res=await axios.post('http://localhost:4000/api/ai/doc',{
+        const res=await axios.post('https://docgenai-3r7d.onrender.com/api/ai/doc',{
             code:code,
             style:style
         })
